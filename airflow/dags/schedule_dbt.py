1295ec5c42383467741cea57b83aa74d960c8a0b
Konstantinos Papadopoulos (Home) <papadopoulospk@gmail.com>
2020-04-06 12:10:25 +0100

    Small clean-up

--- airflow/dags/schedule_dbt.py
+++ airflow/dags/schedule_dbt.py
@@ -10,7 +10,6 @@ import json
 JSON_MANIFEST_DBT = '/project/dbt/target/manifest.json'
 PARENT_MAP = 'parent_map'
 
-
 def sanitise_node_names(value):
         segments = value.split('.')
         if segments[0] == 'model':
@@ -19,17 +18,11 @@ def sanitise_node_names(value):
 def get_node_structure():
     with open(JSON_MANIFEST_DBT) as json_data:
         data = json.load(json_data)
-
     ancestors_data = data[PARENT_MAP]
-
     tree = {}
-
     for node in ancestors_data:
-            
             ancestors = list(set(ancestors_data[node]))
-
             ancestors_2 = [sanitise_node_names(ancestor) for ancestor in ancestors]
-            
             clean_node_name = sanitise_node_names(node)
             if clean_node_name is not None:
                     tree[clean_node_name] = ancestors_2
@@ -47,26 +40,11 @@ default_args = {
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    # 'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
+    'retries': 1
 }
 # [END default_args]
 
 # [START instantiate_dag]
-
 dag = DAG(
     'daily_dbt_models',
     default_args=default_args,
@@ -78,11 +56,8 @@ dag = DAG(
 nodes = get_node_structure()
 operators = {}
 for node in nodes:
-    # date = """{{ macros.ds_format(ts_nodash, "%Y%m%dT%H%M%S", "%Y-%m-%d-%H-%M") }}"""
     date_end = "{{ ds }}"
     date_start = "{{ yesterday_ds }}"
-    # print('MACRO : {}'.format(date))
-    # bsh_cmd = 'cd /project/dbt && dbt run --models {nodeName} --vars \'"start_date":"2020-02-15 15:00:00 UTC", "end_date":"2020-02-15 15:20:00 UTC"}}\' --full-refresh'.format(nodeName = node, date_start = date_start, date_end = date_end)
     bsh_cmd = 'cd /project/dbt && dbt run --models {nodeName} --vars \'{{"start_date":"{start_date}", "end_date":"{end_date}"}}\' '.format(nodeName = node, start_date = date_start, end_date = date_end)
     tmp_operator = BashOperator(
         task_id= node,
